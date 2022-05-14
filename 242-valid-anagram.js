b52b4718cf7da6fd92503e786af8cb4d26c40626
0xTomas <99558481+0xTomas@users.noreply.github.com>
2022-05-14 14:43:46 +1000

    update: added new and improved version

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -5,20 +5,38 @@ const isAnagram = function(s, t) {
    if (s.length !== t.length) return false
 
    const array1 = Array.from({length: 26}, (x, i) => 0);
-   const array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+   // const array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
    for (let i = 0; i < s.length; i++) {
-      let index = array2.indexOf(s[i])
+      let index = s.charCodeAt(i) - 97
       array1[index] += 1
    }
 
    for (let j = 0; j < t.length; j++) {
-      let index = array2.indexOf(t[j])
+      let index = t.charCodeAt(j) - 97
       array1[index] -= 1
    }
-
    return array1.every(x => x === 0)
-
 };
 
-console.log(isAnagram("sant", "ands"))
+/* Runtime: 58 ms, faster than 99.94% of JavaScript online submissions for Valid Anagram.
+ Memory Usage: 42 MB, less than 98.86% of JavaScript online submissions for Valid Anagram.
+
+Added improvements here:
+1. Simpler initialisation of the array
+2. Use of character codes rather than a second array */
+
+const isAnagramFast = function(s, t) {
+   const array1 = new Array(26).fill(0);
+
+   for (let i = 0; i < s.length; i++) {
+      let index = s.charCodeAt(i) - 97
+      array1[index] += 1
+   }
+
+   for (let j = 0; j < t.length; j++) {
+      let index = t.charCodeAt(j) - 97
+      array1[index] -= 1
+   }
+   return array1.every(x => x === 0)
+};
